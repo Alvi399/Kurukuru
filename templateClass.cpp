8c458443bee2d6be71cd80c31ebea62e31187915
Alvi399 <muhammadalvi.23338@mhs.unesa.ac.id>
2024-11-04 17:09:49 +0700

    up

--- templateClass.cpp
+++ templateClass.cpp
@@ -17,8 +17,22 @@ template <typename T> class Array {
 };
 
 template <typename T> Array <T> ::Array(T arr[], int s) {
-    
+    ptr = new T[s];
+    size = s;
+    for (int i = 0; i < size; i++)
+    {
+        ptr[i] = arr[i];
+    }
 }
+
+
+template <typename T> void Array<T>::Print() //cari method void di class array dan pilih fungsi print
+{
+    for (int i = 0; i < size; i++)
+        cout << " " << *(ptr + i);
+    cout << endl;
+}
+
 int main(int argc, char const *argv[])
 {
     bool steatment1 = sort(1,3);
@@ -27,5 +41,9 @@ int main(int argc, char const *argv[])
     cout << steatment1 << endl;
     cout << steatment2 << endl;
     cout << steatment3 << endl;
+
+    int arry[5] = {2,5,7,9,10};
+    Array <int> a(arry,5);
+    a.Print();
     return 0;
 }
